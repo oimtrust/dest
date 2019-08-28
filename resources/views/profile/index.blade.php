65ac33c30eb194117a61cb89903a692ad92d2467
Oim <trustoim@gmail.com>
2019-08-28 22:32:31 +0700

    index of profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -5,150 +5,88 @@ Profile
 @endsection
 
 @section('content')
-<div class="page-header">
-    <h3 class="page-title">
-        My Profile
-    </h3>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-        <li class="breadcrumb-item active" aria-current="page">My Profile</li>
-        </ol>
-    </nav>
+<div class="main-content">
+    <section class="section">
+    <div class="section-header">
+        <h1>Profile</h1>
+    </div>
+
+    <div class="section-body">
+        <div class="col-12 col-sm-12 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="password-tab" data-toggle="tab" href="#password" role="tab" aria-controls="password" aria-selected="true">Update Password</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Name</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" value="{{ $profile->name }}" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Email</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" value="{{ $profile->email }}" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Phone</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" value="{{ $profile->phone }}" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Avatar</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        @if ($profile->avatar)
+                                            <figure class="author-box">
+                                                <img src="{{ asset('stisla/assets/img/avatar/avatar-1.png') }}" alt="..." class="rounded-circle author-box-picture">
+                                            </figure>
+                                        @else
+                                        <input type="text" class="form-control" value="No have avatar" readonly>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Address</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <textarea class="form-control" spellcheck="false" style="margin-top: 0px; margin-bottom: 0px; height: 104px;" readonly>{{ $profile->address }}</textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <button class="btn btn-primary">Update Profile</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="password" role="tabpanel" aria-labelledby="password-tab">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
 </div>
-@if (session('status'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('status') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item">
-      <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="home" aria-selected="true">Profile</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" id="password-tab" data-toggle="tab" href="#password" role="tab" aria-controls="profile" aria-selected="false">Update Password</a>
-    </li>
-  </ul>
-  <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        <div class="row">
-            <div class="col-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-
-                        <form >
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" id="name" name="name" disabled value="{{ $profile->name }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" disabled value="{{ $profile->email }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="phone">Phone</label>
-                                <input type="text" class="form-control" id="phone" disabled value="{{ $profile->phone }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label>Avatar</label> <br/>
-                                @if ($profile->avatar)
-                                <img src="{{ asset('storage/' . $profile->avatar) }}" class="mb-2 rounded" style="width: 200px" alt="image">
-                                @else
-                                <label class="text-info">No Avatar</label>
-                                @endif
-                            </div>
-
-                            <div class="form-group">
-                                <label for="address">Address</label>
-                                <textarea class="form-control" id="address" disabled rows="4">{{ $profile->address }}</textarea>
-                            </div>
-
-                            <a href="{{ route('profile.edit', ['id' => $profile->id]) }}"  class="btn btn-gradient-warning mr-2">Edit</a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="tab-pane fade" id="password" role="tabpanel" aria-labelledby="password-tab">
-        <div class="row">
-            <div class="col-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-
-
-
-                        <form method="POST" action="{{ route('updatePassword', ['id' => $profile->id]) }}">
-                            @csrf
-
-                            <input type="hidden" name="_method" value="PUT">
-
-                            <div class="form-group">
-                                <label for="old_password">Old Password</label>
-                                <div class="input-group">
-                                    <input type="password" class="form-control @error('old_password') is-invalid @enderror" id="old_password" name="old_password">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-sm btn-gradient-primary" onclick="showOldPassword()" type="button">
-                                            <i class="mdi mdi-eye"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                @error('old_password')
-                                <small class="form-text text-danger">
-                                    {{ $message }}
-                                </small>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="new_password">New Password</label>
-                                <div class="input-group">
-                                    <input type="password" class="form-control @error('new_password') is-invalid @enderror" id="new_password" name="new_password">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-sm btn-gradient-primary" onclick="showNewPassword()" type="button">
-                                            <i class="mdi mdi-eye"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                @error('new_password')
-                                <small class="form-text text-danger">
-                                    {{ $message }}
-                                </small>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="password_confirmation">Confirm Password</label>
-                                <div class="input-group">
-                                    <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-sm btn-gradient-primary" onclick="showConfirmPassword()" type="button">
-                                            <i class="mdi mdi-eye"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                @error('password_confirmation')
-                                <small class="form-text text-danger">
-                                    {{ $message }}
-                                </small>
-                                @enderror
-                            </div>
-
-                            <button type="submit"  class="btn btn-gradient-warning mr-2">Update Password</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-
 @endsection
 
 @section('scripts')
